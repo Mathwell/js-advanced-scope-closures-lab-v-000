2a83c54f01d3d49767a35ea9aacd8ae55553d967
Olena Ageyeva <eageeva@hotmail.com>
2018-06-03 00:54:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(range){
    return function(start,end){
-     if (Math.abs(end-start)>range) {
+     let distance=Math.abs(ParseInt(end)-ParseInt(start))
+     if (distance>range) {
        return false
      } else {return `within range by ${range}`}
    }
