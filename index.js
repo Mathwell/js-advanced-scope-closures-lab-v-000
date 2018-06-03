1ae2082e009bbef8825d05e8c984932b10d5373a
Olena Ageyeva <eageeva@hotmail.com>
2018-06-03 01:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ function produceTipCalculator(fare){
 }
 
 function createDriver(){
+  let driverId=0;
   return class{
    constructor(name){
      this.name=name;
+     this.id=driverId++;
    }
   }
 }
